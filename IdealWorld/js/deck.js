71893a3f251d07c1d052bf5bb7789d6e446e4669
日瀬 健人 <kentnewbie@hotmail.com>
2021-06-29 01:30:13 +0900

    T

---
+++ IdealWorld/js/deck.js
@@ -0,0 +1,23 @@
+﻿
+class Deck {
+    _data = [];
+
+    constructor(data = null) {
+        this._data = data;
+    }
+
+    add(card) {
+        this._data.push(card);
+    }    
+    
+    shuffle() {
+        let r = Math.random() * this._data.length;
+        
+    }
+
+    draw() {
+        return this._data.shift();
+    }
+}
+
+export { Deck };
--- IdealWorld/js/main.js
+++ IdealWorld/js/main.js
@@ -1,2 +1,19 @@
-﻿alert("ok");
+﻿import { Deck } from "./deck.js";
 
+(() => {
+    
+    
+    var d = new Deck([]);
+    
+    d.add("a");
+    d.add("b");
+    d.add("c");
+    //console.log(d);
+    //d.shuffle();
+    alert(d.draw());
+    alert(d.draw());
+    //alert(`ok`);
+   /* Deck d = new Deck();*/
+    //d.shffule();
+
+})();
